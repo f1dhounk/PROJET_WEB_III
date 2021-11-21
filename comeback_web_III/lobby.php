648cee0aaa80734ec0284a75c482e3be27e43dae
Fidel Hounkponou <fidhounk@gmail.com>
2021-11-21 13:39:54 -0500

    commit

--- comeback_web_III/lobby.php
+++ comeback_web_III/lobby.php
@@ -10,21 +10,29 @@
 
 
 <div class="container">
+
+	<a href="training.php">
+		<div class="item1" >
+			<img src="img/zoro_training.gif" alt=""></div>
+	</a>
+
+	<a href="fighting.php">
+		<div class="item2" >
+			<img src="img/fight/narutoVSsasuke_fight.gif" alt=""></div>
+	</a>
+
+	<a href="fighting.php">
+		<div class="item3" >
+			<img src="img/leaving/sunny_go_leaving.gif" alt=""> </div>
+	</a>
+
+	<div class="item4" >
+		<iframe style="width:700px;height:240px;" onload="applyStyles(this)" 
+		src="https://magix.apps-de-cours.com/server/#/chat/<?= $_SESSION["key"] ?>">
+		</iframe>
+	</div>
 	
-	<input type="image" src="img/zoro_training.gif" id="training" height="800" width="350"/>
-
-	<input type="image" src="img/fight/narutoVSsasuke_fight.gif" id="fighting" height="650" width="800"/>
-
-	<input type="image" src="img/leaving/sunny_go_leaving.gif" id="leaving" height="800" width="350"/>
-
 </div>
-
-
-
-<iframe style="width:700px;height:240px;" onload="applyStyles(this)" 
-	src="https://magix.apps-de-cours.com/server/#/chat/<?= $_SESSION["key"] ?>">
-</iframe>
-
 
 <?php
 	require_once("lespartial/partial_lobby/footer.php");
