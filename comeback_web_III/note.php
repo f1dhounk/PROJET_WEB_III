de347f3776c737a9a9d6a286d606aad1cd694222
fidhounk <fidhounk@gmail.com>
2021-11-29 17:08:03 -0500

    notes

--- comeback_web_III/note.php
+++ comeback_web_III/note.php
@@ -5,10 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NOTE</title>
+    <link rel="stylesheet" href="css/note.css">
 </head>
 <body>
     
-    
+    <div class="save" ></div>
+
+    <div class="write" >
+        <h1> NOTES / COMMENTAIRES </h1>
+
+        <form action="" method="GET" >
+
+            <div class="answer">
+                
+                <div class="author">
+                    <input type="text" name="auteur" required placeholder="Votre sujet" style="width: 900px; height: 50px;" >
+                </div>
+
+                <div class="author">
+                    <input name="auteur" required placeholder="Votre description" style="width: 900px; height: 50px;" >
+                </div>
+                
+                <div class="text">
+                    <textarea name="text" required placeholder="Vos notes" style="width: 900px; height: 100px;" ></textarea>
+                </div>
+
+                <div class="send-btn">
+                    <a href="envoyer=true"><button>Envoyer</button></a>
+                </div>
+
+            </div>
+
+        </form>
+
+    </div>
 
 </body>
 </html>
